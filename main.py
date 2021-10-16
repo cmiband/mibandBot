908454323d01ddea0815056b3a3e783397079dbd
Crecker <33941168+cmiband@users.noreply.github.com>
2021-10-16 15:22:52 +0000

    bot does something wow

--- main.py
+++ main.py
@@ -2,17 +2,41 @@ import discord
 from googleapiclient.discovery import build
 import os
 
-val = os.environ['TOKEN']
+TOKEN = os.environ['TOKEN']
 ytAPI = os.environ['YT']
 
 userId = "UC_KcYKZtzAgZwXczRcI55lw"
 
+channelsIds = {
+  "Minebarth" : "UC_KcYKZtzAgZwXczRcI55lw",
+  "DZiX" : "UCGd9ZMiV2ZUEdUqMI3oIaEQ",
+  "Gucio" : "gutekwitek"
+}
+
 client = discord.Client()
 youtube = build('youtube','v3',developerKey=ytAPI)
 
-request = youtube.channels().list(part='statisics',id=userId)
-response = request.execute()
-print(response)
+requestMB = youtube.channels().list(part='statistics',id=channelsIds["Minebarth"])
+requestDZ = youtube.channels().list(part='statistics',id=channelsIds["DZiX"])
+requestGU = youtube.channels().list(part='statistics',forUsername=channelsIds["Gucio"])
+responseMB = requestMB.execute()
+responseDZ = requestDZ.execute()
+responseGU = requestGU.execute()
+
+channelsSubscribers = {
+  "Minebarth" : responseMB['items'][0]['statistics']['subscriberCount'],
+  "DZiX" : responseDZ['items'][0]['statistics']['subscriberCount'],
+  "Gucio" : responseGU['items'][0]['statistics']['subscriberCount']
+}
+
+channelsViews = {
+  "Minebarth" : responseMB['items'][0]['statistics']['viewCount'],
+  "DZiX" : responseDZ['items'][0]['statistics']['viewCount'],
+  "Gucio" : responseGU['items'][0]['statistics']['viewCount'],
+}
+
+def getName(msg):
+  return msg.split()[1]
 
 @client.event
 async def on_ready():
@@ -23,7 +47,11 @@ async def on_message(message):
   if message.author == client:
     return
 
-  if message.content.startswith('$hello'):
-    await message.channel.send('Hello!')
+  if any(nick in message.content for nick in channelsIds):
+    nickName = getName(message.content)
+    if message.content.startswith('!subs'):
+        await message.channel.send(nickName+ " ma obecnie " +channelsSubscribers[nickName]+ " subów!")
+    if message.content.startswith('!views'):
+        await message.channel.send(nickName+ " ma obecnie " +channelsViews[nickName]+ " wyświetleń!")
 
-client.run(val)
+client.run(TOKEN)
