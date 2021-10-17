999e4807d3236e1c406823dd50cbff1770bdf00b
Crecker <33941168+cmiband@users.noreply.github.com>
2021-10-17 16:19:21 +0000

    some new features like !montage

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ ytAPI = os.environ['YT']
 client = discord.Client()
 youtube = build('youtube','v3',developerKey=ytAPI)
 
-request = youtube.channels().list(part="snippet",id="UC_KcYKZtzAgZwXczRcI55lw")
-rsp = request.execute()
-
 def getName(msg):
   return msg.split()[2]
 
 def getIdOrUsername(msg):
   return msg.split()[1]
+
+def makeLink(part):
+  return "https://www.youtube.com/watch?v="+part
 
 @client.event
 async def on_ready():
@@ -48,19 +48,20 @@ async def on_message(message):
       else:
         subs = response['items'][0]['statistics']['subscriberCount']
         await message.channel.send(nickName +" ma obecnie "+subs+" subów!")
-    if getIdOrUsername(message.content)!='id' and getIdOrUsername(message.content)!='username':
-      await message.channel.send("Niesprecyzowany filtr szukania")
   
   if message.content.startswith('!views'):
     if getIdOrUsername(message.content)=='id':
       userId = getName(message.content)
       request = youtube.channels().list(part='statistics',id=userId)
+      requestName = youtube.channels().list(part='snippet',id=userId)
       response = request.execute()
+      responseName = requestName.execute()
       if response['pageInfo']['totalResults'] == 0:
         await message.channel.send("Brak wyników(złe id).")
       else:
         views = response['items'][0]['statistics']['viewCount']
-        await message.channel.send(userId +" ma obecnie "+views+" wyświetleń!")
+        channelName = responseName['items'][0]['snippet']['title']
+        await message.channel.send(channelName +" ma obecnie "+views+" wyświetleń!")
     if getIdOrUsername(message.content)=='username':
       nickName = getName(message.content)
       request = youtube.channels().list(part='statistics',forUsername=nickName)
@@ -70,18 +71,27 @@ async def on_message(message):
       else:
         views = response['items'][0]['statistics']['viewCount']
         await message.channel.send(nickName +" ma obecnie "+views+" wyświetleń!")
-    if getIdOrUsername(message.content)!='id' and getIdOrUsername(message.content)!='username':
-      await message.channel.send("Niesprecyzowany filtr szukania")
   
   if message.content.startswith('jebać'):
     await message.channel.send('disa psa orka pana smierci')
 
+  if message.content.startswith('!miband'):
+    await message.channel.send("https://www.youtube.com/channel/UC_KcYKZtzAgZwXczRcI55lw")
+
+  if message.content.startswith('!montage'):
+    request = youtube.playlistItems().list(part="snippet",playlistId="PLUQBK_ASrqgvnii8HJS_k1bfCX7G8KvUm")
+    requestForLink = youtube.playlistItems().list(part='contentDetails',playlistId='PLUQBK_ASrqgvnii8HJS_k1bfCX7G8KvUm')
+    responseForLinkPart = requestForLink.execute()
+    rsp = request.execute() 
+    playlistLen = len(rsp['items'])
+    newestLinkPart = responseForLinkPart['items'][playlistLen-1]['contentDetails']['videoId']
+    newestMontage = rsp['items'][playlistLen-1]['snippet']['title']
+    await message.channel.send("Najnowszy montage: " +newestMontage + "\n" +makeLink(newestLinkPart))
+
   if message.content.startswith('!help'):
     await message.channel.send('!subs id/username argument - subskrypcje(rozjebane na razie)')
     await message.channel.send('!views id/username argument- wyświetlenia(rozjebane na razie)')
-
-  if message.content.startswith('!api'):
-    await message.channel.send(rsp)
-
+    await message.channel.send('!miband - ja B)')
+    await message.channel.send('!montage - najnowszy montage(nie wiem czemu nie wyswietla najnowszego)')
 
 client.run(TOKEN)
